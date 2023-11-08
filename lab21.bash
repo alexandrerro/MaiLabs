96992408c9cf47272dd323feba1cc720d066cc42
alexandrerro <pelmenn2033@gmail.com>
2023-11-08 15:53:02 +0300

    first try bash

---
+++ lab21.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Проверяем введена ли директория 
+if [ $# != 1 ]; then
+    echo "Use: $0 <directory>"
+    exit 1
+fi;
+
+# Проверяем существует ли такая директория
+if [ ! -d "$1" ]; then
+    echo "Ошибка: $1 не директория"
+    exit 1
+fi;
+
+# Traverse directory and output subdirectories
+for file in `find $1 -type d`
+do
+    realpath $file;
+done
+
